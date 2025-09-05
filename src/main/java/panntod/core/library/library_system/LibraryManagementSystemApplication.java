3bab779dacf7db63ebef3e38c2e443cc76bd4328
panntod <munjalindra.pandhu@gmail.com>
2025-09-05 19:02:41 +0700

    feat: Initialize springboot maven project

---
+++ src/main/java/panntod/core/library/library_system/LibraryManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package panntod.core.library.library_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryManagementSystemApplication.class, args);
+	}
+
+}
